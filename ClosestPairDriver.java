f4ad1ce2a7b4d235eb14f517984bc3e607c3801a
Neil Manimtim <31807992+nman03@users.noreply.github.com>
2017-09-19 13:18:59 -0700

    Update ClosestPairDriver.java

--- ClosestPairDriver.java
+++ ClosestPairDriver.java
@@ -1,47 +1,58 @@
 package closestPair;
-import closestPair.ClosestPairAlgorithm.Pair;
-import closestPair.ClosestPairAlgorithm.Point;
+
+import objects.Pair;
+import objects.Point;
 
 public class ClosestPairDriver {
 	
 	public static void main(String[] args) {
 		ClosestPairAlgorithm cp = new ClosestPairAlgorithm();
 		
-		Point p1 = cp.new Point(1, 5);
-		Point p2 = cp.new Point(8, 4);
-		Point p3 = cp.new Point(4, 3);
-		Point p4 = cp.new Point(5, 6);
-		
-		
-		Point[] arr = {p1, p2, p3, p4};
-		
-		Point[] Px = cp.mergeSort(arr, 'x');
-		Point[] Py = cp.mergeSort(arr, 'y');
-		
-		Pair a = cp.ClosestPair(Px, Py);
-	
-		dispArray(Px);
-		dispArray(Py);
+		Point p1 = new Point(1, 6);
+		Point p2 = new Point(15, 7);
+		Point p3 = new Point(6, 1);
+		Point p4 = new Point(7, 15);
+		Point p5 = new Point(5, 8);
+		Point p6 = new Point(8, 5);
+		Point p7 = new Point(0, 0);
 			
-		Pair b = cp.new Pair(p1, p2);
-		Pair c = cp.new Pair(p1, p3);
-		Pair d = cp.new Pair(p1, p4);
-		Pair e = cp.new Pair(p2, p3);
-		Pair f = cp.new Pair(p2, p4);
-		Pair g = cp.new Pair(p3, p4);
+		Point[] arr = {p1, p2, p3, p4, p5, p6, p7};
+		Pair a = cp.closestPair(arr);
 		
-		System.out.println(a.toString());
-		System.out.println(b.toString());
-		System.out.println(c.toString());
-		System.out.println(d.toString());
-		System.out.println(e.toString());
-		System.out.println(f.toString());
-		System.out.println(g.toString());	
+		// All possible Pairs (for testing)
+//		System.out.println(new Pair(p1, p2).toString());
+//		System.out.println(new Pair(p1, p3).toString());
+//		System.out.println(new Pair(p1, p4).toString());
+//		System.out.println(new Pair(p1, p5).toString());
+//		System.out.println(new Pair(p1, p6).toString());
+//		System.out.println(new Pair(p1, p7).toString());
+//		System.out.println(new Pair(p2, p3).toString());
+//		System.out.println(new Pair(p2, p4).toString());
+//		System.out.println(new Pair(p2, p5).toString());
+//		System.out.println(new Pair(p2, p6).toString());
+//		System.out.println(new Pair(p2, p7).toString());
+//		System.out.println(new Pair(p3, p4).toString());
+//		System.out.println(new Pair(p3, p5).toString());
+//		System.out.println(new Pair(p3, p6).toString());
+//		System.out.println(new Pair(p3, p7).toString());
+//		System.out.println(new Pair(p4, p5).toString());
+//		System.out.println(new Pair(p4, p6).toString());
+//		System.out.println(new Pair(p4, p7).toString());
+//		System.out.println(new Pair(p5, p6).toString());
+//		System.out.println(new Pair(p5, p7).toString());
+//		System.out.println(new Pair(p6, p7).toString());
+
+		dispArray(arr);
+		System.out.println("The closest pair of Points: " + a.toString());
 	}
 
 	public static void dispArray(Point[] array) {
+		System.out.print("Points: {");
 		for (int i = 0 ; i < array.length ; i++) {
-			System.out.print(array[i].toString() + " ");
+			if (i == array.length - 1)
+				System.out.print(array[i] + "}");
+			else 
+				System.out.print(array[i] + ", ");
 		}
 		System.out.println();
 	}
